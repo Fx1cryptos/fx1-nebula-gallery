d54b79d99fc79f06888a3af3e7af390aaf5f3ae0
FX1 Digital Hubs <Fxcrypto1invest@gmail.com>
2025-10-27 21:29:04 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { BaseProvider } from './providers/BaseProvider'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-createRoot(document.getElementById("root")!).render(
-  <BaseProvider>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </BaseProvider>
-);
+  </React.StrictMode>
+)
