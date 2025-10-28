7b6de06fadfb4de3bdd7973fb32311d93ba3102c
FX1 Digital Hubs <Fxcrypto1invest@gmail.com>
2025-10-27 21:31:23 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { useEffect } from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { sdk } from "@farcaster/miniapp-sdk"; // Base Mini App SDK
+
 import Index from "./pages/Index";
 import Gallery from "./pages/Gallery";
 import CreateArt from "./pages/CreateArt";
@@ -20,33 +23,43 @@ import FX1Flux from "./pages/FX1Flux";
 import Tokens from "./pages/Tokens";
 import NotFound from "./pages/NotFound";
 
-const App = () => (
-  <TooltipProvider>
-    <Toaster />
-    <Sonner />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/create" element={<CreateArt />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/stake" element={<Stake />} />
-        <Route path="/trade" element={<Trade />} />
-        <Route path="/runway" element={<Runway />} />
-        <Route path="/wear-to-earn" element={<WearToEarn />} />
-        <Route path="/referrals" element={<Referrals />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/social" element={<Social />} />
-        <Route path="/rewards" element={<Rewards />} />
-        <Route path="/leaderboard" element={<Leaderboard />} />
-        <Route path="/arena" element={<Arena />} />
-        <Route path="/fx1-flux" element={<FX1Flux />} />
-        <Route path="/tokens" element={<Tokens />} />
-        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  </TooltipProvider>
-);
+const App = () => {
+  useEffect(() => {
+    // Signal to Base Mini App that the app is ready
+    sdk.actions
+      .ready()
+      .then(() => console.log("FX1 Mini App is ready on Base!"))
+      .catch((err) => console.error("Mini App ready error:", err));
+  }, []);
+
+  return (
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/create" element={<CreateArt />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/stake" element={<Stake />} />
+          <Route path="/trade" element={<Trade />} />
+          <Route path="/runway" element={<Runway />} />
+          <Route path="/wear-to-earn" element={<WearToEarn />} />
+          <Route path="/referrals" element={<Referrals />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/social" element={<Social />} />
+          <Route path="/rewards" element={<Rewards />} />
+          <Route path="/leaderboard" element={<Leaderboard />} />
+          <Route path="/arena" element={<Arena />} />
+          <Route path="/fx1-flux" element={<FX1Flux />} />
+          <Route path="/tokens" element={<Tokens />} />
+          {/* Catch-all route */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  );
+};
 
 export default App;
