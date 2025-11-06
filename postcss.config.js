43a061adbabf8adce7ff94f6f90bb6fae5421e17
FX1 Digital Hubs <Fxcrypto1invest@gmail.com>
2025-11-06 09:38:10 -0600

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
+// postcss.config.js
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    // optional: add future plugins if needed
+    // 'postcss-nested': {},
+    // 'postcss-preset-env': { stage: 1 },
   },
-}
+};
